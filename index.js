31a6425dad0ee75993dfe0d6a21ebbb85201e1b8
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-04-23 00:08:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import express from 'express';
 import pino from 'pino';
-import { default as makeWASocket, useMultiFileAuthState, DisconnectReason, Browsers, makeCacheableSignalKeyStore } from '@whiskeysockets/baileys';
+import makeWASocket, {
+  useMultiFileAuthState,
+  DisconnectReason,
+  Browsers,
+  makeCacheableSignalKeyStore,
+} from '@whiskeysockets/baileys';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const SESSION_DIR = path.join(__dirname, 'session');
